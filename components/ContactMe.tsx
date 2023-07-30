3c4bd749dbce74aac873565e57bd179c8591ba8e
lunarmoon <49crehbgr@gmail.com>
2023-07-30 17:41:27 +0900

    style: change css

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -23,32 +23,32 @@ export default function ContactMe({}: Props) {
   }
 
   return (
-    <div className='h-screen relative flex flex-col text-cetner md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <div className='max-w-375px h-screen relative flex flex-col text-cetner sm:text-left sm:flex-row sm:max-w-7xl md:px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Contact
       </h3>
-      <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>
+      <div className='flex flex-col space-y-10 w-full items-center'>
+        <h4 className='text-2xl sm:text-4xl font-semibold text-center'>
           I have got just what you need.{' '}
-          <span className='decoration-[#f7ab0a]/50 underline'>Lets Talk.</span>
+          <span className='block sm:inline decoration-[#f7ab0a]/50 underline'>Lets Talk.</span>
         </h4>
 
-        <div className='space-y-10'>
+        <div className='space-y-5 flex flex-col items-start justify-center'>
           <div className='flex items-center justify-center space-x-5'>
             <PhoneIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>+1065777996</p>
+            <p className='text-lg sm:text-2xl'>+1065777996</p>
           </div>
           <div className='flex items-center justify-center space-x-5'>
             <EnvelopeIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>zentechie7@gmail.com</p>
+            <p className='text-lg sm:text-2xl'>zentechie7@gmail.com</p>
           </div>
-          <div className='flex items-center justify-center space-x-5'>
+          {/* <div className='flex items-center justify-center space-x-5'>
             <PhoneIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>123 Developer Lane</p>
-          </div>
+            <p className='text-lg sm:text-2xl'>123 Developer Lane</p>
+          </div> */}
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
+        <form onSubmit={handleSubmit(onSubmit)} className='w-full flex flex-col space-y-2 sm:w-fit mx-auto'>
           <div className='flex space-x-2'>
             <input {...register('name')} placeholder='Name' className='contactInput' type='text' />
             <input {...register('email')} placeholder='Email' className='contactInput' type='email' />
@@ -57,7 +57,7 @@ export default function ContactMe({}: Props) {
           <textarea {...register('message')} placeholder='Message' className='contactInput' />
           <button
             type='submit'
-            className='bg-[#f7ab0a] pt-5 px-10 rounded-md text-black font-bold text-lg'
+            className='bg-[#f7ab0a] py-3 px-10 rounded-md text-black font-bold text-lg'
           >
             Submit
           </button>
