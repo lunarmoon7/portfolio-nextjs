008e8c1f529ca652b8370df45c51b7fb98aea9db
lunarmoon <49crehbgr@gmail.com>
2023-08-03 19:11:06 +0900

    bug:

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ type Props = {
   projects: Project[];
   socials: Social[];
 }
-export default async function() {
+export default async function Home() {
   const { pageInfo, experiences, skills, projects, socials } = await getData();
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
