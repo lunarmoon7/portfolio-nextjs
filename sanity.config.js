f5829d5ee3b9ac285aa1cd24ddea9a3ac71b7214
lunarmoon <49crehbgr@gmail.com>
2023-08-03 19:01:59 +0900

    bug: deploy 안되는 버그

---
+++ sanity.config.js
@@ -0,0 +1,22 @@
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.jsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schema'
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId: "oud9556n",
+  dataset: "production",
+  schema,
+  plugins: [
+    deskTool(),
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
